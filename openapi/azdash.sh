adcac7275710b57dfeab3cd7978747529f0666a1
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-10-07 00:24:41 +0200

    create dash

--- openapi/azdash.sh
+++ openapi/azdash.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# 1 - retivere del openapi
+# 1 - retrive del openapi
+echo "1 - retrive del openapi"
 
-# 2 - crea il file cfg
+# 2 - create cfg file
+echo "2 - create cfg file"
 cat <<EOF > config.yaml
 oa3_spec: https://raw.githubusercontent.com/pagopa/pagopa-iuv-generator/fix-deploy/openapi/openapi.json
-name: [UAT] IUV Generation service
+name: UAT IUV Generation service
 location: West Europe
 timespan: 5m
 resources:
   - /subscriptions/26abc801-0d8f-4a6e-ac5f-8e81bcc09112/resourceGroups/pagopa-u-vnet-rg/providers/Microsoft.Network/applicationGateways/pagopa-u-app-gw
 EOF
 
-
-# 3 - crea la dash
-docker run -v $(pwd):/home/nonroot/resources:Z \
-  ghcr.io/pagopa/opex-dashboard:latest generate \
-  --template-name azure-dashboard \
-  --config-file /home/nonroot/resources/config.yaml > uat_dash_gps.json
+# 3 - create dash
+echo "3 - create dash"
+docker run -v $(PWD):/home/nonroot/resources:Z ghcr.io/pagopa/opex-dashboard:latest generate --template-name azure-dashboard --config-file /home/nonroot/resources/config.yaml > uat_dash.json
